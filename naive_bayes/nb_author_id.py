fa3dd07f07a5f9d2d223617fb8f5c8e0c715386a
Kordian D <kordiandrozd@gmail.com>
2017-07-23 19:23:58 +0200

    SVM completed

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,13 +14,14 @@ import sys
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 from time import time
+from sklearn.naive_bayes import GaussianNB
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-from sklearn.naive_bayes import GaussianNB
+
 
 classifier = GaussianNB()
 
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import svm
 
 
 ### features_train and features_test are the features for the training
@@ -21,10 +22,25 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
+classifier = svm.SVC(kernel='rbf', C=10000)
 
-#########################################################
-### your code goes here ###
 
-#########################################################
+t0 = time()
+classifier.fit(features_train, labels_train)
 
+print("training time:", round(time()-t0, 3), "s")
+
+t1 = time()
+prediction = classifier.predict(features_test)
+
+cris = 0
+for i in prediction:
+    if i == 1:
+        cris += 1
+
+print(cris)
+
+print("predicting time:", round(time()-t1, 3), "s")
+
+print(classifier.score(features_test, labels_test))
 
